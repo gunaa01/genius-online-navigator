8425a00cd931ea9bd7feb720e017be78650cf30a
kamal <guna2030a@gmail.com>
2025-04-26 16:49:43 +0530

    Revert "Fix: Resolve 'vite not found' error"
    This reverts commit 144407598a23543b844f84ddb65be0d34ea52985.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -10,6 +11,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 );
+=======
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById("root")!).render(<App />);
+>>>>>>> parent of 1444075 (Fix: Resolve 'vite not found' error)
 =======
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
